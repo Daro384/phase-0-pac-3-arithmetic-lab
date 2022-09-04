5670f081b9f64b19d9c786a49ad534977354434e
Joseph <joffan98@gmail.com>
2022-09-03 20:05:03 -0500

    commit tree dd54b77a098b92d7f852cd902dc1f6507d175f90

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(num1, num2){
+    return num1 + num2
+}
+
+function subtract(num1, num2){
+    return num1 - num2
+}
+
+function multiply(factor1, factor2){
+    return factor1 * factor2
+}
+
+function divide(quotient, divisor){
+    return quotient / divisor
+}
+
+function increment(number) {
+    return ++number
+}
+
+function decrement(number) {
+    return --number
+}
+
+function makeInt(notAnInt) {
+    return parseInt(notAnInt, 10)
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n, 10)
+}
